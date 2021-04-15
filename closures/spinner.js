892348358f6f38167459c032031cc9ff911b5fab
Magesh Kuppan <tkmagesh77@gmail.com>
2021-04-15 18:20:44 +0530

    spinner - assignment

---
+++ closures/spinner.js
@@ -0,0 +1,22 @@
+var spinner = /*  */
+//up(), down()
+
+spinner.up() //=> 1
+spinner.up() //=> 2
+spinner.up() //=> 3
+spinner.up() //=> 4
+
+spinner.down() //=> 3
+spinner.down() //=> 2
+spinner.down() //=> 1
+spinner.down() //=> 0
+spinner.down() //=> -1
+
+
+The user should NOT be able to influence the outcome of 'up' and 'down' methods from outside
+
+spinner.count = 10000
+spinner.up() //=> SHOULD NOT return 10001
+
+count = 10000
+spinner.up() //=> SHOULD NOT return 10001
