5cd0e98b37c1df7b9c016f122020a72af09a050a
Magesh Kuppan <tkmagesh77@gmail.com>
2021-04-16 13:48:01 +0530

    prototypal inheritance

--- constructor-functions/demo.js
+++ constructor-functions/demo.js
@@ -24,3 +24,18 @@ function Employee(id, name, salary){
         console.log('ID = ', this.id, ' name = ', this.name, ' salary = ', this.salary);
     };
 }
+
+
+function Employee(id, name, salary){
+    if (this.constructor !== Employee){
+        return new Employee(id, name, salary);
+    }
+    //this -> new object
+    this.id = id;
+    this.name = name;
+    this.salary = salary;
+    //this -> returned by default
+}
+Employee.prototype.display = function(){
+    console.log('ID = ', this.id, ' name = ', this.name, ' salary = ', this.salary);
+};
